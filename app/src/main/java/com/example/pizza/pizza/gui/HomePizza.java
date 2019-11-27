d235a83a5c2f663c0f0f875596e0ec86116d7476
Ana <anappn19@gmail.com>
2019-11-27 19:46:19 -0300

    adicionando botão

--- app/src/main/java/com/example/pizza/pizza/gui/HomePizza.java
+++ app/src/main/java/com/example/pizza/pizza/gui/HomePizza.java
@@ -2,9 +2,13 @@ package com.example.pizza.pizza.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pizza.R;
+import com.example.pizza.infra.gui.MainActivity;
 
 public class HomePizza extends AppCompatActivity {
 
@@ -14,5 +18,14 @@ public class HomePizza extends AppCompatActivity {
         setContentView(R.layout.activity_home_pizza);
         getSupportActionBar().hide();
 
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePizza.this, MainActivity.class));
+
+            }
+        });
     }
 }
